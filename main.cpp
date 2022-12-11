67c84f7a0be1a8345a88da993e5a8aef419eac04
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-11 22:43:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
-#include "lib.h"
 using namespace std;
 
 
+float max(const float array[],int d) {
+    int max, i;
+    max = array[0];
+    for (i = 1; i < d; i++) {
+
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
+}
+
 int main() {
     int a, c;
-
+    
     cin >> a;
     float array[a];
     for (int i = 0; i < a; i++) {
-
-
 
         cin >> array[i];
     }
