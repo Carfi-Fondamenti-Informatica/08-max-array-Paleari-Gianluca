cee7c7e01504fc692a0d1eced7a451991014028e
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-11 23:33:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,13 @@
 #include "lib.h"
 
-
-
-
-float max(const float array[], int d) {
-    int m, i;
-    m = array[0];
+float max(const float array[],int d) {
+    float max;
+    int i;
+    max = array[0];
     for (i = 1; i < d; i++) {
 
-        if (array[i] > m)
-            m = array[i];
+        if (array[i] > max)
+            max = array[i];
     }
-    return m;
+    return max;
 }
-  
-  
