226cb9bfa7de8e36d7b4329d153d4ffaefbad08a
Paleari-Gianluca <118019843+Paleari-Gianluca@users.noreply.github.com>
2022-12-11 22:33:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    int a, c;
+    cout << "inserire dimensione array" << endl;
+    cin >> a;
+    float array[a];
+    for (int i = 0; i < a; i++) {
+
+
+        cout << "inserire elemento:";
+        cin >> array[i];
+    }
+
+
+    cout<<"massimo:"<< max(array,a);
+
+    return 0;
+
 }
